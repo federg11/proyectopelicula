3031069d76263112ac9297905732d6b6f3f4a015
Federico <federicorg07@gmail.com>
2020-05-14 20:23:53 -0300

    cambios

--- pelicula-backend/routes/peliculas.js
+++ pelicula-backend/routes/peliculas.js
@@ -3,76 +3,97 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './imagenes');
+    },
+    filename: function (req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+      cb(null, name);
+    }
+  });
+  const upload = multer({ storage: storage })
 
 const PeliculaSchema = new Schema(
     {
     _id: ObjectId,
     titulo: {type: String, trim: true},
     descripcion: String,
-    comentarios: String
+    comentarios: String,
+    imagen: String
   },
   {timestamps:true}
   );
 
   const PeliculaModel = mongoose.model('PeliculaModel', PeliculaSchema);
 
-router.get('/', async (req,res) => { 
-        try {
-            const respuesta = await PeliculaModel.find();
-            res.json({mensaje: "Listado de peliculas", pelicula: respuesta});
-        } catch (error) {
-            res.status(505).json({mensaje:"error", tipo: error});
-        }
-});
-
-router.get('/:idPelicula', async (req,res) => {
-    const id = req.params.idPelicula;
+  const obtenerPeliculas = async (req,res) => { 
     try {
-        const respuesta = await PeliculaModel.findById(id);
-        res.json({mensaje: "Pelicula encontrada", pelicula: respuesta});
+        const respuesta = await PeliculaModel.find();
+        res.json({mensaje: "Listado de peliculas", pelicula: respuesta});
     } catch (error) {
         res.status(505).json({mensaje:"error", tipo: error});
     }
-});
+};
 
-router.post('/', async (req,res) =>{
-    const peliculaNueva = new PeliculaModel({
-        _id: new ObjectId(),
-        titulo: req.body.titulo,
-        descripcion: req.body.descripcion,
-        comentarios: req.body.comentarios
-    });
-    try {
-        const respuesta = await peliculaNueva.save();
-        res.json({mensaje:"Pelicula creada con exito", pelicula: respuesta});
-    } catch (error) {
-        res.status(505).json({mensaje: "error", tipo: err});
-    }    
-});
+    const obtenerPelicula = async (req,res) => {
+        const id = req.params.idPelicula;
+        try {
+            const respuesta = await PeliculaModel.findById(id);
+            res.json({mensaje: "Pelicula encontrada", pelicula: respuesta});
+        } catch (error) {
+            res.status(505).json({mensaje:"error", tipo: error});
+        }
+    };
 
-router.put('/:idPelicula', async (req,res) => {
-    //modifico una pelicula, la localizo por ID (req.params.idPelicula)
-    //y sobreescribo el contenido, ese contenido esta en req.body
-    const id= req.params.idPelicula;
-    const peliculaModificada = req.body;
-    try {
-        const respuesta = await PeliculaModel.findByIdAndUpdate(id,peliculaModificada);
-        res.json({mensaje:"la pelicula fue modificada", pelicula: respuesta});
-    } catch (error) {
-        res.status(505).json({mensaje: "error", tipo: error});
-    }
-    
-});
+    const crearPelicula = async (req,res) =>{
+        console.log('req: ',req.file.filename);
+        const urlImagen = 'http://localhost:3000/imagenes/' + req.file.filename;
+        const peliculaNueva = new PeliculaModel({
+            _id: new ObjectId(),
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            comentarios: req.body.comentarios,
+            imagen: urlImagen //eso lo genero multer al crear la imagen en la carpta
+        });
+        try {
+            const respuesta = await peliculaNueva.save();
+            res.json({mensaje:"Pelicula creada con exito", pelicula: respuesta});
+        } catch (error) {
+            res.status(505).json({mensaje: "error", tipo: err});
+        }    
+    };
 
-router.delete('/:idPelicula', async (req,res) =>{
-    // eliminar pelicula
-    const id= req.params.idPelicula;
-    try {
-        const respuesta = await PeliculaModel.findByIdAndDelete(id);
-        res.json({mensaje:"la pelicula fue eliminada", pelicula: respuesta});
-    } catch (error) {
-        res.status(505).json({mensaje: "error", tipo: error});
-    }
-});
+    const modificarPelicula = async (req,res) => {
+        //modifico una pelicula, la localizo por ID (req.params.idPelicula)
+        //y sobreescribo el contenido, ese contenido esta en req.body
+        const id= req.params.idPelicula;
+        const peliculaModificada = req.body;
+        try {
+            const respuesta = await PeliculaModel.findByIdAndUpdate(id,peliculaModificada);
+            res.json({mensaje:"la pelicula fue modificada", pelicula: respuesta});
+        } catch (error) {
+            res.status(505).json({mensaje: "error", tipo: error});
+        }
+        
+    };
+
+    const eliminarPelicula = async (req,res) =>{
+        // eliminar pelicula
+        const id= req.params.idPelicula;
+        try {
+            const respuesta = await PeliculaModel.findByIdAndDelete(id);
+            res.json({mensaje:"la pelicula fue eliminada", pelicula: respuesta});
+        } catch (error) {
+            res.status(505).json({mensaje: "error", tipo: error});
+        }
+    };
+
+router.get('/', obtenerPeliculas);
+router.get('/:idPelicula', obtenerPelicula);
+router.post('/',upload.single("imagen"),crearPelicula);
+router.put('/:idPelicula', modificarPelicula);
+router.delete('/:idPelicula', eliminarPelicula);
 
 module.exports= router;
--- pelicula-backend/routes/users.js
+++ pelicula-backend/routes/users.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
